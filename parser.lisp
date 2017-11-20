a321f3caba335a82c155ea9b44f1a5b0d575fd6c
ccQpein <ccqpein@protonmail.com>
2017-11-20 15:36:37 -0500

    start

---
+++ parser.lisp
@@ -0,0 +1,13 @@
+(defvar *test* "(a b (c))")
+(defvar *test1* "(a b (c))")
+
+;;:= parser may not need symbol table.
+;; symbol table using in compiling to find symbol scope
+(defvar *symbol-table* (make-hash-table))
+
+(defun match-char (char))
+
+
+(defun scan-code-block (code))
+
+
