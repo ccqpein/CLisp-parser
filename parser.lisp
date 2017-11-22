7d761149e99d01f67d368b3916393bd7492aedb9
ccQpein <ccqpein@protonmail.com>
2017-11-21 21:50:28 -0500

    a bit

--- parser.lisp
+++ parser.lisp
@@ -1,13 +1,25 @@
 (defvar *test* "(a b (c))")
-(defvar *test1* "(a b (c))")
+(defvar *test1* "(a b (c d (e 4))")
 
 ;;:= parser may not need symbol table.
 ;; symbol table using in compiling to find symbol scope
 (defvar *symbol-table* (make-hash-table))
+(defvar *scope-stack* '())
 
-(defun match-char (char))
+(defun match-char (char symbol-stack)
+  ;:= could use char= in future
+  (cond ((equal char #\()
+	 ;:= could use tail recursive
+	 (cons (gensym) symbol-stack))
+	))
 
 
-(defun scan-code-block (code))
+(defun scan-code-block (code)
+  (let ((stack '()))
+    (loop 
+       for c in code
+       do (setf stack (match-char c stack))
+	 )
+    stack))
 
 
