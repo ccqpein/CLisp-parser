eda641b05d0f8c4a434b4af155e24347731e2092
ccQpein <ccqpein@protonmail.com>
2017-11-29 16:29:49 -0500

    a bit

--- parser.lisp
+++ parser.lisp
@@ -2,21 +2,28 @@
 (defvar *test1* "(a b (c d (e 4))")
 (defvar *test2* "( ( aa (a) a aa ))))")
 
-;;:= parser may not need symbol table.
-;; symbol table using in compiling to find symbol scope
-(defvar *symbol-table* (make-hash-table))
+
+(defvar *scope-table* (make-hash-table))
+(defvar *scope-dependency-table* (make-hash-table))
 (defvar *scope-stack* '())
 
-(defun match-char (char symbol-stack)
-  ;:= could use char= in future
-  (cond ((equal char #\()
-	 ;:= could use tail recursive
-	 (cons (gensym) symbol-stack))
-	(t symbol-stack)
-	))
-
+(defun scan-and-update-scope (elis stack table dep)
+  (print elis)
+  (cond ((eql #\( (car elis))
+	 (scan-and-update-scope ; tail recursive here
+	  (cdr elis)
+	  (push (gensym) stack)
+	  (setf (gethash (car stack) table) '())
+	  (setf (gethash (car stack) table) (append (gethash (car stack) table) (list (car stack))))))
+	((eql #\) (car elis))
+	 (scan-and-update-scope
+	  (cdr elis)
+	  (cdr stack)
+	  table
+	  dep))))
 
 (defun scan-code-block (code)
+  "cut code to a list including all symbol/word"
   (loop
      with result = '()
      with temp = '()
