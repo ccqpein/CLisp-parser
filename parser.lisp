cc96798396979a19ef377d8a711d0ef4f2e01b24
ccQpein <ccqpein@protonmail.com>
2017-12-05 16:24:59 -0500

    a bit

--- parser.lisp
+++ parser.lisp
@@ -1,27 +1,34 @@
 (defvar *test* "(a b (c))")
 (defvar *test1* "(a b ((c d (e 4)))")
 (defvar *test2* "( ( aa (a) a aa ))))")
+(defvar *test3* "( ( aa (a) a aa )) (c d (b) (d))")
 
 
-(defvar *scope-table* (make-hash-table))
-(defvar *scope-dependency-table* (make-hash-table))
+(defvar *scope-table* (make-hash-table :test 'equal))
+(defvar *scope-dependency-table* (make-hash-table :test 'equal))
 (defvar *scope-stack* (list (make-symbol "Adam")))
 
+
+(defun update-list-in-hashtabe (key table &rest eles)
+  (setf (gethash key table)
+	(append (gethash key table) eles)))
+
 (defun scan-and-update-scope (elis stack table dep)
-  ; stack no side effection
-  (print (list elis stack table dep))
   (cond ((eql nil elis)
-	 nil)
+	 stack)   ; setf no side effect, so return stack to keep stack state
 	((eql #\( (car elis)) ;; need more test
 	 (scan-and-update-scope
 	  (cdr elis)
-	  (progn (setf stack (push (gensym) stack))
-		 (setf (gethash (symbol-name (cadr stack)) table) (append (gethash (symbol-name (cadr stack)) table) (list (car stack))))
-		 stack)
-	  (progn (setf (gethash (symbol-name (car stack)) table) '())
-		 table)
-	  (progn (setf (gethash (symbol-name (car stack)) dep) (append (gethash (symbol-name (car stack)) dep) (list (car stack))))
-		 dep)))
+	  (progn
+	    (setf stack (push (gensym) stack))
+	    (update-list-in-hashtabe (symbol-name (cadr stack)) table (car stack))
+	    stack) ; need some other function
+	  (progn
+	    (setf (gethash (symbol-name (car stack)) table) '())
+	    table)
+	  (progn 
+	    (update-list-in-hashtabe (symbol-name (cadr stack)) dep (car stack))
+	    dep)))
 	((eql #\) (car elis))
 	 (scan-and-update-scope
 	  (cdr elis)
@@ -32,8 +39,9 @@
 	 (scan-and-update-scope
 	  (cdr elis)
 	  stack
-	  (progn (setf (gethash (symbol-name (car stack)) table) (append (gethash (symbol-name (car stack)) table) (list (car elis))))
-		 table)
+	  (progn 
+	    (update-list-in-hashtabe (symbol-name (car stack)) table (car elis))
+	    table)
 	  dep))))
 
 (defun scan-code-block (code)
@@ -62,11 +70,11 @@
      finally (return-from scan-code-block result)))
 
 
-(setf *scope-table* (make-hash-table)
-      *scope-dependency-table* (make-hash-table)
+(setf *scope-table* (make-hash-table :test 'equal)
+      *scope-dependency-table* (make-hash-table :test 'equal)
       *scope-stack* (list (make-symbol "Adam"))
 )
-(scan-and-update-scope (scan-code-block *test1*)
+(scan-and-update-scope (scan-code-block *test3*)
 		       *scope-stack*
 		       *scope-table*
 		       *scope-dependency-table*)
