e207f24da2340ee69b4bc110976116b9fd7dfbfc
ccQpein <ccqpein@protonmail.com>
2018-03-17 12:50:40 -0400

    add code && add test function

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # CLisp-parser
 
-`common-lisp` parse lisp file && create a file including two tables. 
+## Summary ##
 
+`Clisp-parser` is a lite tool to parse common lisp code (maybe works with other lisp too, I haven't tried).
+
+`Clisp-parser` read lisp file and create code struct in two tables:
 ~~~lisp
 *scope-table*
 *scope-dependency-table*
@@ -15,5 +18,39 @@ Every s-expression has a symbol, this symbol be a key of scope-table to point to
 
 I try to make this table for symbol depend relationship to find symbol scope. However, it looks like `go-translater` do not need find symbols scope.
 
+#### Example ####
+
+If you code like this
+~~~lisp
+;; root scope named "Adam"
+( ( aa (a) a aa )) 
+
+(c d (b) (d))
+~~~
+
+Two tables will like this:
+~~~
+"scope-table" 
+("Adam" (#:G683 #:G686)) 
+("G683" (#:G684)) 
+("G684" ("aa" #:G685 "a" "aa")) 
+("G685" ("a")) 
+("G686" ("c" "d" #:G687 #:G688)) 
+("G687" ("b")) 
+("G688" ("d")) 
+"scope-dependency-table" 
+("G683" (#:|Adam|)) 
+("G684" (#:G683 #:|Adam|)) 
+("G685" (#:G684 #:G683 #:|Adam|)) 
+("G686" (#:|Adam|)) 
+("G687" (#:G686 #:|Adam|)) 
+("G688" (#:G686 #:|Adam|)) 
+~~~
 
 ## Usage ##
+
+> Only can handle one file so far
+
+`./main.lisp filename`
+
+then there is a `.ccq` file created in folder
--- test/test.lisp
+++ test/test.lisp
@@ -10,7 +10,44 @@
 (defvar *test3* "( ( aa (a) a aa )) (c d (b) (d))")
 (defvar *test4* "(( da ee da ) ; )))")
 
-(read-code "./testcase0.lisp")
+(defun test-parse-flow (case)
+  (let ((*scope-table* (make-hash-table :test 'equal))
+	(*scope-dependency-table* (make-hash-table :test 'equal))
+	(stack (list (make-symbol "Adam"))))
+    (scan-and-update-scope (scan-code-block case)
+			   stack
+			   *scope-table*
+			   *scope-dependency-table*)
+    (print "stack")
+    (print stack)
+    (print "scope-table")
+    (loop
+      for key being the hash-keys of *scope-table*
+	using (hash-value value)
+       do (print (list key value)))
+    (print "scope-dependency-table")
+    (loop
+       for key being the hash-keys of *scope-dependency-table*
+       using (hash-value value)
+       do (print (list key value)))
+    ))
+
+;(test-parse-flow *test1*)
+;(test-parse-flow *test2*)
+;(test-parse-flow *test3*)
+;(test-parse-flow *test4*)
+(defun read-file-test ()
+  (read-code "./testcase0.lisp")
+  (print "scope-table")
+  (loop
+     for key being the hash-keys of parser-io::*scope-table*
+     using (hash-value value)
+     do (print (list key value)))
+  (print "scope-dependency-table")
+  (loop
+     for key being the hash-keys of parser-io::*scope-dependency-table*
+     using (hash-value value)
+     do (print (list key value))))
 
 (write-ccq-file parser-io::*scope-table*
 		parser-io::*scope-dependency-table*)
